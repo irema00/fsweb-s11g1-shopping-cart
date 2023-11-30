19af4c25dc3e7792924164b9e3fd52cbd73aa742
İrem Akın Tunga <iremakin00@gmail.com>
2023-12-01 01:13:20 +0300

    adds localStorage

--- src/contexts/CartContext.js
+++ src/contexts/CartContext.js
@@ -1,9 +1,16 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useState, useEffect } from "react";
 
 export const CartContext = createContext();
 
 export function CartContextProvider({ children }) {
-  const [cart, setCart] = useState([]);
+  const [cart, setCart] = useState(() => {
+    const savedCart = localStorage.getItem("cart");
+    return savedCart ? JSON.parse(savedCart) : [];
+  });
+
+  useEffect(() => {
+    localStorage.setItem("cart", JSON.stringify(cart));
+  }, [cart]);
   return (
     <CartContext.Provider value={{ cart, setCart }}>
       {children}
