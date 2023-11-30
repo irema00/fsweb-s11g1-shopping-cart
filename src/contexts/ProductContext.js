86dcaaba482f253a432def3f338eb18aa7e0eff5
İrem Akın Tunga <iremakin00@gmail.com>
2023-12-01 00:14:42 +0300

    adds ProductContext

--- src/contexts/ProductContext.js
+++ src/contexts/ProductContext.js
@@ -1,7 +1,23 @@
-import { createContext } from "react";
+import { createContext, useState, useEffect, useContext } from "react";
+import { CartContext } from "./CartContext";
+import { data } from "../data";
 
 export const ProductContext = createContext();
 
 export function ProductContextProvider({ children }) {
-  return <ProductContext.Provider>{children}</ProductContext.Provider>;
+  const [products, setProducts] = useState(data);
+  const { cart, setCart } = useContext(CartContext);
+
+  const addItem = (item) => {
+    setCart([...cart, item]);
+  };
+  useEffect(() => {
+    console.log("SEPETE EKLEDİM Mİ?", cart);
+  }, [cart]);
+
+  return (
+    <ProductContext.Provider value={{ products, addItem }}>
+      {children}
+    </ProductContext.Provider>
+  );
 }
